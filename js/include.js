0f77508ed0ec1ee0d719dbf0ddcfabce4fb1be3a
Eight <380080496@qq.com>
2018-03-08 17:02:53 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -45,10 +45,12 @@ $(function(){
     }else if($(window).width() < 1024){
       $(".user_img").css({"width": "60px","height": "60px","margin-left": "35%"});
     }
+
     //相册手机适配
     if($(window).width() == 414){
       $(".li_info").css({"margin-left": "25%"});
     }
+    
 
 	 //启用滚动条
 	$(document.body).css({"overflow-x":"auto","overflow-y":"auto"});
